b7575fbb4ac2d524be03a5366f5d628e496ffff3
kevely10 <163428636+kevely10@users.noreply.github.com>
2024-05-07 10:09:54 -0300

    Add files via upload

---
+++ restaurante.py
@@ -0,0 +1,15 @@
+class Restaurante:
+def__init__(self,nome.categoria):
+    self,nome = nome
+    self,categoria = categoria
+    self,ativo = False
+
+def__str__(self):
+    return self.nome
+
+restaurante_praca = Restaurante('praça','Gourmet')
+restaurante_pizza = Restaurante('pizza', 'Italiana')
+
+restaurantes = [restaurante_pizza.restaurante_praca]
+
+print(restaurante_praca)
